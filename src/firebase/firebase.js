b4cf317964bea2ef0a3121117e264021407beaec
Euan Morgan <euanmorgan48@gmail.com>
2021-03-22 04:36:11 +0000

    update vulnerable dependencies

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 const firebase = require("firebase");
-console.log(process.env);
+
 const firebaseApp = firebase.default.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
