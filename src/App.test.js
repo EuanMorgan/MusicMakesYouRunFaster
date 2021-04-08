ece123cbdbf32c47f1046f98d18a8ec93c52bfc8
Euan Morgan <euanmorgan48@gmail.com>
2021-04-08 12:55:40 +0100

    reenable test

--- src/App.test.js
+++ src/App.test.js
@@ -2,20 +2,20 @@
 // import App from './App';
 
 //basic test to test circleci integration
-import { addTwoNumbers } from "./TestTest";
-test("Adds 3 + 2 to equal 5", () => {
-  expect(addTwoNumbers(3, 2)).toBe(5);
-});
+// import { addTwoNumbers } from "./TestTest";
+// test("Adds 3 + 2 to equal 5", () => {
+//   expect(addTwoNumbers(3, 2)).toBe(5);
+// });
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-// import { parseSongsAndRun } from "./Functions/MainApiCalls";
-// import { run, songs, shouldBe } from "./TestData";
-// test("Parsing run function", async () => {
-//   let x = await parseSongsAndRun(songs, run.run_map, "7LZHNM", true);
+import { parseSongsAndRun } from "./Functions/MainApiCalls";
+import { run, songs, shouldBe } from "./TestData";
+test("Parsing run function", async () => {
+  let x = await parseSongsAndRun(songs, run.run_map, "7LZHNM", true);
 
-//   expect(x).toStrictEqual(shouldBe);
-// });
+  expect(x).toStrictEqual(shouldBe);
+});
